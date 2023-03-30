ec835a6063c62084506bbdbdeac96e2bfea78ef0
Minh Binh <>
2023-03-30 13:44:41 +0700

    commit

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  SERVER_KEY: '123456789ABCXyzsdfdsf5345345dsfDSFSDF',
-  SERVER_URL: 'https://api.enow.vn',
-  CDN_URL: 'http://cdn.enow.vn',
-  GOOGLE_KEY: 'AIzaSyAr68TY24XHylszFsJk17BAzCSi0Mo10Zg',
-  VER: 'vi',
-  TIME_INTERVAL_DEFAULT: 5000,
-  TIME_INTERVAL_TIMEOUT: 3000,
-};
--- src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-export const COMMON_CONST = {
-  PageSize: 10,
-  FormatDate: 'DD-MM-YYYY',
-  FormatDateTime: 'DD/MM/YYYY - HH:mm',
-  FormatDateServer: 'YYYY-MM-DD',
-  FormatDateTimeServer: 'YYYY-MM-DD HH:mm:ss',
-};
